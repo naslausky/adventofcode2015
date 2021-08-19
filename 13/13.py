469921bf653e5317f5a8a07eaba6748b2f0bca07
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2021-08-19 19:24:41 -0300

    Adiciona o segundo meio de resolução ao décimo terceiro dia, de maneira análoga ao nono dia.

--- 13/13.py
+++ 13/13.py
@@ -1,6 +1,7 @@
 #Desafio do dia 13/12/2015:
 #a) Receber um grafo e organizar os nós de maneira circular de forma a maximizar a soma dos valores
-#b) Adicionar um nó de conexões nulas e re-calcular o resultado.
+#b) Adicionar um nó de conexões nulas e re-calcular o resultado. 
+# Provavelmente é equivalente a calcular o nó com menor contribuição a soma e remover ele.
 import itertools
 
 dicionarioAlegria = {} #Dicionário da forma {Ana : {Bob:32,Carol:12}...}
@@ -24,15 +25,29 @@ def contabilizarAlegriaTotal (listaPessoas):
 		nomePessoa = listaPessoas[indicePessoa]
 		nomePessoaAnterior = listaPessoas[indicePessoa-1]
 		nomePessoaPosterior = listaPessoas[(indicePessoa+1) % len(listaPessoas)] 
-
 		alegriaTotal += dicionarioAlegria[nomePessoa][nomePessoaAnterior]
 		alegriaTotal += dicionarioAlegria[nomePessoa][nomePessoaPosterior]
 	return alegriaTotal
-#Tentei igual o dia 9, mas não funcionou, provavelmente porque precisaria avaliar os dois lados na iteração
 maiorAlegriaTotal = max([contabilizarAlegriaTotal(permutacao) 
 			for permutacao in itertools.permutations(dicionarioAlegria)])
 print('A maior variação de alegria possível é:', maiorAlegriaTotal)
-
+########### Fazendo sem usar iterTools, da mesma forma que foi feito o dia 9. Obtém a resposta correta, mas acredito que é porque o input é "bonzinho":
+#maioresAlegrias = {}
+#for pessoaInicial, alegrias in dicionarioAlegria.items():
+#	ordemAteAgora = [pessoaInicial]
+#	while len(ordemAteAgora) != len(dicionarioAlegria):
+#		alegriaMaxima = -10000000
+#		pessoaAtual = ordemAteAgora[-1]
+#		candidatos = [x for x,_ in dicionarioAlegria.items() if x not in ordemAteAgora]
+#		for candidato in candidatos:
+#			alegriaPossivel = dicionarioAlegria[pessoaAtual][candidato] + dicionarioAlegria[candidato][pessoaAtual]
+#			if alegriaPossivel > alegriaMaxima and (candidato not in ordemAteAgora):
+#				alegriaMaxima = alegriaPossivel
+#				candidatoComMaiorAlegria = candidato
+#		ordemAteAgora.append(candidatoComMaiorAlegria)
+#	maioresAlegrias[pessoaInicial] = ordemAteAgora
+#print(max([contabilizarAlegriaTotal(x) for _, x in maioresAlegrias.items()]))
+###########
 #Parte2:
 meuNome = 'Naslausky' 
 minhasAlegrias = {}
@@ -40,7 +55,6 @@ for nome,alegrias in dicionarioAlegria.items():
 	alegrias[meuNome] = 0 #Adiciona o novo a todos os dicionários das outras pessoas
 	minhasAlegrias[nome] = 0
 dicionarioAlegria[meuNome] = minhasAlegrias #Adiciona o meu próprio dicionário de alegrias ao dicionário pai.
-
 maiorAlegriaTotal = max([contabilizarAlegriaTotal(permutacao) 
 			for permutacao in itertools.permutations(dicionarioAlegria)])
 print('A maior variação de alegria possível comigo incluso é:', maiorAlegriaTotal)
