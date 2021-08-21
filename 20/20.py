c8956c2218ae76cc02dbeece7569121035a855f4
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2021-08-21 02:08:13 -0300

    Refatora o vigésimo dia para não repetir código entre a primeira e segunda partes.

--- 20/20.py
+++ 20/20.py
@@ -1,6 +1,6 @@
 #Desafio do dia 20/12/2015
 #a) Calcular o menor número cuja soma de seus fatores é maior do que um dado número.
-#b) Idem, porém cada fator só é contado para números 
+#b) Idem, porém cada fator só é contado para números.
 import math
 with open('input.txt') as file:
 	numeroMinimo = int(file.read().splitlines()[0])
@@ -17,16 +17,14 @@ def menorNumeroDeCasa(numeroMinimo, Parte2 = False):
 		elfoInicial = 1
 		for numeroElfo in range(elfoInicial,limiteSuperior+1):
 			if (numeroCasa % numeroElfo) == 0:
-				if Parte2:
-					if (numeroCasa/numeroElfo) < 50:
-						numeroDePresentes += numeroElfo * valorDeCadaElfo
-					if numeroCasa != numeroElfo**2:
-						if (numeroCasa/numeroElfo > numeroCasa/50):
-							numeroDePresentes+= int(numeroCasa/numeroElfo) * valorDeCadaElfo
-				else:
-					numeroDePresentes += numeroElfo*valorDeCadaElfo
-					if numeroCasa != numeroElfo**2: #Se ele chegar exatamente na raiz contaria 2x
-						numeroDePresentes += int(numeroCasa / numeroElfo) * valorDeCadaElfo
+				candidatos = (numeroElfo, int(numeroCasa/numeroElfo))
+				soma = 0
+				if candidatos[0] == candidatos[1]: #Quando for a exata raiz, não calcular 2x
+					candidatos = [candidatos[0]]
+				for candidato in candidatos:
+					if not Parte2 or (candidato > (int(numeroCasa/50))):
+							soma+=candidato
+				numeroDePresentes += soma * valorDeCadaElfo	
 	return numeroCasa
 print("Casa de menor valor a alcançar", numeroMinimo, "presentes:", menorNumeroDeCasa(numeroMinimo))
 print("Com os elfos entregando até 50 casas, a casa de menor valor é:", menorNumeroDeCasa(numeroMinimo, True))
