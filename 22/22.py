18d17948b87f2c158949e8b708b3d5d43232b848
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2021-08-24 13:19:56 -0300

    Adiciona a resolução funcional de ambas as partes do vigésimo segundo dia.

--- 22/22.py
+++ 22/22.py
@@ -1,6 +1,6 @@
 #Desafio do dia 22/12/2015
-#a) Dada uma lista de magias, ver qual o mínimo da mana necessário para matar o boss
-#b) 
+#a) Dada uma lista de magias, ver qual o mínimo da mana necessário para matar o boss.
+#b) Idem porém sempre perdendo 1 de vida a cada começo de turno seu.
 inimigo = {}
 with open('input.txt') as file:
 	linhas = file.read().splitlines()
@@ -8,6 +8,7 @@ with open('input.txt') as file:
 		chave, valor = linha.split(': ')
 		inimigo[chave] = int(valor)
 	inimigo['Poison Turns'] = 0
+
 
 movimentos = [
 {'Cost': 53, 'Damage': 4, 'Heal': 0, 'Shield Turns': 0, 'Poison Turns': 0,'Regen Turns': 0},
@@ -17,125 +18,116 @@ movimentos = [
 {'Cost': 229, 'Damage': 0, 'Heal': 0, 'Shield Turns': 0, 'Poison Turns': 0,'Regen Turns': 5},
 ]
 
-def batalhar(jogador, inimigo, manaGastaAteAgora): #Roda um turno do jogador, um turno do inimigo, e segue. Retorna se ganhou, e quanto de mana gastou no mínimo
-	print('Iniciando Turno')
-	print(jogador,'\n',inimigo, manaGastaAteAgora)
-	input()
-	menorManaGastaQueGanha = 100000
-	consegueGanhar = False
-	movimentosQuePodemSerExecutados = [movimento for movimento in movimentos if movimento['Cost'] < jogador['Mana']]
-	for movimento in movimentosQuePodemSerExecutados:
+def verificarTerminoBatalha(jogador, inimigo): #Método que verifica se o jogo acabou. Retorna um booleano para tal, e outro para indicar quem ganhou.
+	if inimigo['Hit Points'] <= 0:
+		return True, True #Acabou, inimigo morreu
+	if jogador['Hit Points'] <= 0:
+		return True,False #Acabou, jogador morreu
+	return False, False #Não acabou
 
-		jogadorNesteTurno = jogador.copy() #Para não atrapalhar as outras recursões
+def turnoDoInimigo(jogador, inimigo):
+
+	#Verifica as condições especiais:
+	verificarEfeitosAtuais(jogador,inimigo)
+
+	acabou,jogadorVenceu = verificarTerminoBatalha(jogador, inimigo) #Verifica se ele morreu nesse veneno.
+	if acabou: #O inimigo morreu envenenado.
+		return acabou, jogadorVenceu
+
+	#Ataque do inimigo:
+	danoCausadoPeloInimigo = inimigo['Damage'] - jogador['Armor']
+	danoCausadoPeloInimigo = max(danoCausadoPeloInimigo, 1) #Como o dano do meu input é maior que 7, nem precisaria dessa linha, eu acho.
+	jogador['Hit Points'] -= danoCausadoPeloInimigo
+	return verificarTerminoBatalha(jogador, inimigo)
+
+def verificarEfeitosAtuais(jogador,inimigo): #Verifica, aplica, e reduz os contadores os efeitos atuais do jogador e do inimigo.
+	if jogador['Shield Turns']>0: #Armadura.
+		jogador['Armor'] = 7
+		jogador['Shield Turns'] -= 1
+	else:
+		jogador['Armor'] = 0
+
+	if jogador['Regen Turns'] > 0: #Regeneração de mana.
+		jogador['Regen Turns'] -= 1
+		jogador['Mana'] += 101
+
+	if inimigo['Poison Turns'] > 0: #Veneno.
+		inimigo['Hit Points'] -= 3
+		inimigo['Poison Turns'] -= 1
+
+minimoAteAgora = 100000
+def batalhar(jogador, inimigo, manaGastaAteAgora, Parte2 = False): #Roda um turno do jogador, um turno do inimigo, e segue. Retorna se ganhou, e quanto de mana gastou até o final vitorioso mais econômico.
+
+	global minimoAteAgora
+	if manaGastaAteAgora > minimoAteAgora: # Se ele já gastou mais do que uma outra vitória gastou, já para por aqui.
+		return False, -1
+
+	if Parte2: #No modo Hard, antes de todo turno seu, você perde 1 de vida.
+		jogador['Hit Points'] -= 1
+		acabou, _ = verificarTerminoBatalha(jogador,inimigo)
+		if acabou:
+			return False, -1
+
+	verificarEfeitosAtuais(jogador,inimigo) #Começa verificando os efeitos deles.
+
+	estadosVitoriosos = [] #Lista de inteiros que representam as manas gastas que trouxeram a vitória.
+
+	movimentosQuePodemSerExecutados = [movimento for movimento in movimentos if movimento['Cost'] <= jogador['Mana']] #Só pode executar os que ele tem mana suficiente.
+
+	for movimento in movimentosQuePodemSerExecutados:
+		jogadorNesteTurno = jogador.copy() #Pra não atrapalhar os outros movimentos
 		inimigoNesteTurno = inimigo.copy()
 
-		def verificarTerminoBatalha():
-			if inimigoNesteTurno['Hit Points'] <= 0:
-				return True, True #Acabou, inimigoMorreu
-			if jogadorNesteTurno['Hit Points'] <= 0:
-				return True,False #Acabou, jogador morreu
-			return False, False #Não acabou
+		manaDesteMovimento = movimento['Cost'] #Diminui a mana gasta neste movimento e incrementa a mana total gasta até aqui.
+		manaTotalAteEsteTurno = manaGastaAteAgora + manaDesteMovimento
 
-		def turnoDoInimigo():
-			if inimigoNesteTurno['Poison Turns'] > 0:
-				inimigoNesteTurno['Hit Points'] -= 3
-				inimigoNesteTurno['Poison Turns'] -= 1
-			ganhou,jogadorVenceu = verificarTerminoBatalha()
-			if ganhou: #Morreu envenenado
-				return ganhou, jogadorVenceu
-			danoCausadoPeloInimigo = inimigoNesteTurno['Damage'] - jogadorNesteTurno['Armor']
-			danoCausadoPeloInimigo = max(danoCausadoPeloInimigo, 1) #O dano mínimo é 1. O dano do meu input é maior que 7, nem precisa.
-			jogadorNesteTurno['Hit Points'] -= danoCausadoPeloInimigo
-			return verificarTerminoBatalha()
-		
-		##Verifica os efeitos atuais do jogador
-		if jogadorNesteTurno['Shield Turns']>0:
-			jogadorNesteTurno['Armor'] = 7
-			jogadorNesteTurno['Shield Turns'] -= 1
-		else:
-			jogadorNesteTurno['Armor'] = 0
+		jogadorNesteTurno['Mana'] -= manaDesteMovimento #Executou o movimento, gastou mana.
 
-		if jogadorNesteTurno['Regen Turns'] > 0:
-			jogadorNesteTurno['Regen Turns'] -= 1
-			jogadorNesteTurno['Mana'] += 101
-
-		if movimento['Damage'] > 0: #MM ou Life Drain
+		if movimento['Damage'] > 0: #Magic Missile ou Life Drain.
 			inimigoNesteTurno['Hit Points'] -= movimento['Damage'] 
 			jogadorNesteTurno['Hit Points'] += movimento['Heal']
-			manaTotal = manaGastaAteAgora + movimento['Cost']
-			jogadorNesteTurno['Mana'] -= movimento['Cost']
-			acabou, inimigoMorreu = verificarTerminoBatalha()
-			if acabou:
-				if inimigoMorreu:
-					consegueGanhar = True
-					menorManaGastaQueGanha = min(manaTotal, menorManaGastaQueGanha)
-			else: #Turno do inimigo:
-				acabou,jogadorVivo = turnoDoInimigo()
-				if acabou: #Ou morreu envenenado, ou o hit do boss matou o jogador.
-					if jogadorVivo:
-						consegueGanhar = True
-						menorManaGastaQueGanha = min(manaTotal, menorManaGastaQueGanha)
-				else: #segue a batalha
-					ganhou,menorManaDaquiPraFrente = batalhar(jogadorNesteTurno, inimigoNesteTurno, manaTotal)
-					if ganhou:
-						consegueGanhar = True
-						menorManaGastaQueGanha = min(menorManaDaquiPraFrente, menorManaGastaQueGanha)
-					
 
-		elif movimento['Shield Turns']>0:
-			if jogadorNesteTurno['Shield Turns'] > 0:
+		elif movimento['Shield Turns']>0: #Shield.
+			if jogadorNesteTurno['Shield Turns'] > 0: #Não pode usar essa magia enquanto o efeito ainda estiver ativo
 				continue
 			else:
 				jogadorNesteTurno['Shield Turns'] = movimento['Shield Turns']
-				manaTotal = manaGastaAteAgora + movimento['Cost']
-				jogadorNesteTurno['Mana'] -= movimento['Cost']
-				acabou,jogadorVivo = turnoDoInimigo()
-				if acabou: #Ou morreu envenenado, ou o hit do boss matou o jogador.
-					if jogadorVivo:
-						consegueGanhar = True
-						menorManaGastaQueGanha = min(manaTotal, menorManaGastaQueGanha)
-				else:
-					ganhou, menorManaDaquiPraFrente = batalhar(jogadorNesteTurno, inimigoNesteTurno, manaTotal)
-					if ganhou:
-						consegueGanhar = True
-						menorManaGastaQueGanha = min(menorManaDaquiPraFrente, menorManaGastaQueGanha)
+				jogadorNesteTurno['Armor'] = 7
 
-		elif movimento['Poison Turns'] > 0:
+		elif movimento['Poison Turns'] > 0: #Poison.
 			if inimigoNesteTurno['Poison Turns'] > 0:
 				continue
 			else:
 				inimigoNesteTurno['Poison Turns'] = movimento['Poison Turns']
-				manaTotal = manaGastaAteAgora + movimento['Cost']
-				jogadorNesteTurno['Mana'] -= movimento['Cost']
-				acabou,jogadorVivo = turnoDoInimigo()
-				if acabou: #Ou morreu envenenado, ou o hit do boss matou o jogador.
-					if jogadorVivo:
-						consegueGanhar = True
-						menorManaGastaQueGanha = min(manaTotal, menorManaGastaQueGanha)
-				else:
-					ganhou, menorManaDaquiPraFrente = batalhar(jogadorNesteTurno, inimigoNesteTurno, manaTotal)
-					if ganhou:
-						consegueGanhar = True
-						menorManaGastaQueGanha = min(menorManaDaquiPraFrente, menorManaGastaQueGanha)
-		else: #Regen
+
+		else: #Regenação de mana.
 			if jogadorNesteTurno['Regen Turns'] > 0:
 				continue
 			else:
 				jogadorNesteTurno['Regen Turns'] = movimento['Regen Turns']
-				manaTotal = manaGastaAteAgora + movimento['Cost']
-				jogadorNesteTurno['Mana'] -= movimento['Cost']
-				acabou,jogadorVivo = turnoDoInimigo()
-				if acabou: #Ou morreu envenenado, ou o hit do boss matou o jogador.
-					if jogadorVivo:
-						consegueGanhar = True
-						menorManaGastaQueGanha = min(manaTotal, menorManaGastaQueGanha)
-				else:
-					ganhou, menorManaDaquiPraFrente = batalhar(jogadorNesteTurno, inimigoNesteTurno, manaTotal)
-					if ganhou:
-						consegueGanhar = True
-						menorManaGastaQueGanha = min(menorManaDaquiPraFrente, menorManaGastaQueGanha)
-	return consegueGanhar, menorManaGastaQueGanha
+
+		acabou, jogadorVenceu = verificarTerminoBatalha(jogadorNesteTurno, inimigoNesteTurno) #Pode ter matado no hit no caso do Magic Missile ou do Life Drain.
+		if acabou:
+			if jogadorVenceu: #Venceu neste turno, neste movimento. Gastou só a mana deste movimento:
+				estadosVitoriosos.append(manaTotalAteEsteTurno)
+
+		else: #Segue o baile com o turno do inimigo:
+			acabou, jogadorVenceu = turnoDoInimigo(jogadorNesteTurno, inimigoNesteTurno)
+			if acabou: #O inimigo pode ter morrido envenenado, ou matado o jogador no ataque
+				if jogadorVenceu: #Venceu sem nem precisar turnos seguintes.
+					estadosVitoriosos.append(manaTotalAteEsteTurno)
+			else: #Segue a batalha:
+				ganhou, manaVitoriosa = batalhar(jogadorNesteTurno, inimigoNesteTurno, manaTotalAteEsteTurno, Parte2)
+				if ganhou:
+					estadosVitoriosos.append(manaVitoriosa)
+	if estadosVitoriosos:
+		menorManaVitoriosaDesteTurno = min(estadosVitoriosos)
+		minimoAteAgora = min(minimoAteAgora, menorManaVitoriosaDesteTurno)
+		return True, menorManaVitoriosaDesteTurno #É possível ganhar daqui pra frente, com no mínimo tal mana.
+	else:
+		return False, -1
 
 jogadorInicial = {'Hit Points' : 50, 'Mana': 500, 'Armor': 0, 'Regen Turns':0, 'Shield Turns': 0}
-print(batalhar(jogadorInicial, inimigo, 0)) #1235 too high
-
+print('Mínimo de mana necessário para ganhar:', batalhar(jogadorInicial, inimigo, 0)[1])
+minimoAteAgora = 100000 #Reinicia o valor mínimo que serve como limitador de recursão.
+print('Mínimo de mana necessário para ganhar no modo hard:', batalhar(jogadorInicial, inimigo, 0, True)[1])
